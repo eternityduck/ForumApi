5e9856edf2ece70de5d6b1ec47c883c5482d10bd
bohdan <bogdan2003bk96@gmail.com>
2021-09-06 11:45:54 +0300

    wannaDiev2

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using DAL.Models;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPostRepository Posts { get;}
+        ICommentRepository Comments { get;}
+        IUserRepository Users { get;}
+        Task<int> SaveAsync();
+    }
+}
---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.Models;
+using DAL.Repositories;
+
+namespace DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ForumContext _context;
+        public IPostRepository Posts { get; }
+        public ICommentRepository Comments { get; }
+        public IUserRepository Users { get; }
+        
+
+        public UnitOfWork(ForumContext context)
+        {
+            _context = context;
+            Posts = new PostRepository(_context);
+            Comments = new CommentRepository(_context);
+            Users = new UserRepository(_context);
+        }
+        
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
