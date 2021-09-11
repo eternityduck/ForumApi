c80c7d3660f1ae12a655f198cbf08b3297b8b30d
bohdan <bogdan2003bk96@gmail.com>
2021-09-11 23:28:45 +0300

    fixedSearch

--- BLL/AutoMapperProfile.cs
+++ BLL/AutoMapperProfile.cs
@@ -7,17 +7,17 @@ namespace BLL
 {
     public class AutoMapperProfile : Profile
     {
-        // public AutoMapperProfile()
-        // {
-        //     CreateMap<Post, PostModel>()
-        //         .ForMember(p => p.Comments, c =>
-        //             c.MapFrom(posts => posts.Comments))
-        //         // .ForMember(p => p.AuthorName, c => 
-        //         //     c.MapFrom(x => x.Author.UserName))
-        //         .ReverseMap();
-        //     CreateMap<Comment, CommentModel>()
-        //         .ReverseMap();
-        //     CreateMap<User, UserModel>().ReverseMap();
-        // }
+        public AutoMapperProfile()
+        {
+            CreateMap<Post, PostModel>()
+                .ForMember(p => p.Comments, c =>
+                    c.MapFrom(posts => posts.Comments))
+                // .ForMember(p => p.AuthorName, c => 
+                //     c.MapFrom(x => x.Author.UserName))
+                .ReverseMap();
+            CreateMap<Comment, CommentModel>()
+                .ReverseMap();
+            CreateMap<User, UserModel>().ReverseMap();
+        }
     }
 }
