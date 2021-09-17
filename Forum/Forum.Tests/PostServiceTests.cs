6462d64bc4d61e4d6c1fb23f305d3b5f54808d76
bohdan <bogdan2003bk96@gmail.com>
2021-09-17 13:41:49 +0300

    tests

---
+++ Forum/Forum.Tests/PostServiceTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Forum.Tests
+{
+    public class PostServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
