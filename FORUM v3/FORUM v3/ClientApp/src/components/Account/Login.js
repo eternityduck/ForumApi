4f92479f8bc01c3762e1412df4494f37dbac2660
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2021-12-06 17:39:50 +0300

    Delete FORUM v3 directory

--- FORUM v3/FORUM v3/ClientApp/src/components/Account/Login.js
+++
@@ -1,3 +0,0 @@
-import authService from './api-authorization/AuthorizeService'
-
-
--- FORUM v3/FORUM v3/ViewModels/UserViewModel/CreateUserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Forum.ViewModels.UserViewModel
-{
-    public class CreateUserViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Name { get; set; }
-    }
-}
