b0fb5c6ef8be8261168dbaba0559e14c6f0a055e
Bohdan Karashchuk <70644164+DiscofromKPI@users.noreply.github.com>
2022-05-13 21:25:32 +0300

    Delete ForumWebMVC directory

--- ForumWebMVC/ForumWebMVC/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Forum.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- ForumWebMVC/ForumWebMVC/ViewModels/UserViewModel/EditUserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Forum.ViewModels.UserViewModel
-{
-    public class EditUserViewModel
-    {
-        public string Id { get; set; }
-        public string Email { get; set; }
-        public string UserName { get; set; }
-    }
-}
